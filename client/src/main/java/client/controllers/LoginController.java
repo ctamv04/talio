5675790a059b950f7ce8e5b6bb26ab759ca1f892
Ilse Inanc <I.A.N.Inanc@student.tudelft.nl>
2023-03-24 11:40:52 +0100

    Added back button to login page and fixed checkstyle

--- client/src/main/java/client/controllers/LoginController.java
+++ client/src/main/java/client/controllers/LoginController.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -26,6 +27,8 @@ public class LoginController implements Initializable {
     @FXML
     private Button join_board_button;
     @FXML
+    private Button back_button;
+    @FXML
     private TextField code_input;
     @FXML
     private ListView<Board> boards;
@@ -44,6 +47,16 @@ public class LoginController implements Initializable {
         this.mainCtrl = mainCtrl;
     }
 
+    /***
+     * Initializes the login page
+     * @param location
+     * The location used to resolve relative paths for the root object, or
+     * {@code null} if the location is not known.
+     *
+     * @param resources
+     * The resources used to localize the root object, or {@code null} if
+     * the root object was not localized.
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -70,45 +83,66 @@ public class LoginController implements Initializable {
         });
 
         boards.setOnMouseClicked(event -> {
-            Board board = boards.getSelectionModel().getSelectedItem();
-
-            if (board != null) {
-                Long clickedBoardID = board.getId();
-
-                if (event.getClickCount() == 2) {
-                    mainCtrl.showClientOverview(clickedBoardID);
-                }
-
-                buttonBox.setOpacity(1L);
-                enterBoard.setOnMouseClicked(event2 -> {
-                    mainCtrl.showClientOverview(clickedBoardID);
-                });
-
-                delBoard.setOnMouseClicked(event2 -> {
-                    serverUtils.deleteBoard(clickedBoardID);
-                });
-            }
+            boardClicked(event);
         });
 
         join_board_button.setOnAction(event -> {
-            try {
-                Long id = Long.parseLong(code_input.getText());
-                Board board = serverUtils.getBoard(id);
-                mainCtrl.showClientOverview(serverUtils.getBoard(id).getId());
-            } catch (NumberFormatException | WebApplicationException e) {
-
-                invalid_text.setVisible(true);
-            }
+            joinButtonClicked();
         });
 
         new_board_button.setOnAction(event -> mainCtrl.showAddBoardPage());
+
+        back_button.setOnMouseClicked(event -> {
+            serverUtils.setServer("http://localhost:8080/");
+            mainCtrl.showStartingPage();
+        });
 
         window.setOnMouseClicked(event -> {
 
             if (event.getTarget() != buttonBox && event.getTarget() != boards) {
                 buttonBox.setOpacity(0L);
             }
-        });
-    }
-}
+        }); //window.setOnMouseClicked
+    } // initialize
+
+    /***
+     * This function is called when something happens with the board.
+     * @param event the mouse event that happened: clicked/clicked twice/...
+     */
+    public void boardClicked (MouseEvent event) {
+        Board board = boards.getSelectionModel().getSelectedItem();
+
+        if (board != null) {
+            Long clickedBoardID = board.getId();
+
+            if (event.getClickCount() == 2) {
+                mainCtrl.showClientOverview(clickedBoardID);
+            }
+
+            buttonBox.setOpacity(1L);
+            enterBoard.setOnMouseClicked(event2 -> {
+                mainCtrl.showClientOverview(clickedBoardID);
+            });
+
+            delBoard.setOnMouseClicked(event2 -> {
+                serverUtils.deleteBoard(clickedBoardID);
+            });
+        }
+    } // boardClicked
+
+    /***
+     * This is called when the join button is clicked.
+     */
+    public void joinButtonClicked () {
+        try {
+            Long id = Long.parseLong(code_input.getText());
+            Board board = serverUtils.getBoard(id);
+            mainCtrl.showClientOverview(serverUtils.getBoard(id).getId());
+        } catch (NumberFormatException | WebApplicationException e) {
+
+            invalid_text.setVisible(true);
+        }
+    } // joinButtonClicked
+
+} // LoginController
 
