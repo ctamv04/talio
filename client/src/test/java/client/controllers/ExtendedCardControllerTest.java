91c55ba16084a67161f916e03c6add73281592e8
ChristosTamvakas <C.Tamvakas@student.tudelft.nl>
2023-03-25 17:36:56 +0100

    Added ExtendedCardController tests

---
+++ client/src/test/java/client/controllers/ExtendedCardControllerTest.java
@@ -0,0 +1,43 @@
+package client.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExtendedCardControllerTest {
+
+    /**
+     *
+     */
+    @Test
+    void initialize() {
+    }
+
+    /**
+     *
+     */
+    @Test
+    void back() {
+    }
+
+    /**
+     *
+     */
+    @Test
+    void save() {
+    }
+
+    /**
+     *
+     */
+    @Test
+    void titleHoverIn() {
+    }
+
+    /**
+     *
+     */
+    @Test
+    void titleHoverOut() {
+    }
+}
